b3cfe16538b1e2afb74914e7f604cc4b89fcd89f
Jacob <jakeywaldrip@gmail.com>
2024-01-31 02:03:11 -0600

    Added a tiny bit

--- src/room/room_spawning.rs
+++ src/room/room_spawning.rs
@@ -1,13 +1,17 @@
 
 use screeps::{
   game,
-  Room
+  Room,
+  StructureSpawn
 };
 
-use log::*;
-
 pub fn run_spawning(room: &mut Room) {
+  log::debug!("Running spawns for room {}", room.name());
+  for spawn in game::spawns().values() {
+    run_spawn(&spawn);
+  }
+}
 
-  debug!("Running room spawning {}", room.name());
-
+pub fn run_spawn(spawn: &StructureSpawn) {
+  log::debug!("Running spawn for {}", spawn.name());
 }
